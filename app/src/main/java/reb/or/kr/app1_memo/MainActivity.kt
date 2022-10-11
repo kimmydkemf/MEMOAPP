08c5592a1639f14b784348e839f7e2762a3a9a2e
Dounselor <kimmydkemf@gmail.com>
2022-10-11 12:37:12 +0900

    Splash 기능 구현 - xml 이해 조금 더 필요(color와 drawble 겹쳤을때 가려지는 현상)

--- app/src/main/java/reb/or/kr/app1_memo/MainActivity.kt
+++ app/src/main/java/reb/or/kr/app1_memo/MainActivity.kt
@@ -2,6 +2,7 @@ package reb.or.kr.app1_memo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import reb.or.kr.app1_memo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 딜레이 시간을 주어 splash 화면이 좀더 보여지게 한다.
+        SystemClock.sleep(1000)
+
+        // Splash 화면 이후로 보여질 화면의 테마를 설정한다.
+        setTheme(R.style.Theme_App1_Memo)
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
