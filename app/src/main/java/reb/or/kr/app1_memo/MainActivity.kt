04793884b26bfd648108ef6b1488575dd95e3b80
Dounselor <kimmydkemf@gmail.com>
2022-10-17 21:40:58 +0900

    git 메모 저장 및 보여주기 기능 작업 연습

--- app/src/main/java/reb/or/kr/app1_memo/MainActivity.kt
+++ app/src/main/java/reb/or/kr/app1_memo/MainActivity.kt
@@ -4,13 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import reb.or.kr.app1_memo.databinding.ActivityMainBinding
+import reb.or.kr.app1_memo.databinding.MainRecyclerRowBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityMainBinding
+
+    // 제목을 담을 ArrayList
+    val subject_list=ArrayList<String>()
+    // 작성 날짜를 담을 ArrayList
+    val date_list = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +38,53 @@ class MainActivity : AppCompatActivity() {
         // Toolbar를 설정한다.
         setSupportActionBar(binding.mainToolbar)
         title="메모앱"
+
+        // RecyclerView 셋팅
+        val main_recycler_adapter = MainRecyclerAdapter()
+        binding.mainRecycler.adapter=main_recycler_adapter
+        binding.mainRecycler.layoutManager=LinearLayoutManager(this)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // ArrayList를 비워준다.
+        subject_list.clear()
+        date_list.clear()
+
+        // 데이터 베이스 오픈
+        val helper =DBHelper(this)
+
+        // 쿼리문
+        val sql="""
+            select memo_subject,memo_date
+            from MemoTable
+            order by memo_idx desc
+        """.trimIndent()
+
+        val c1=helper.writableDatabase.rawQuery(sql,null)
+
+        while(c1.moveToNext()){
+            // 컬럼 index를 가져온다.
+            val idx1=c1.getColumnIndex("memo_subject")
+            val idx2=c1.getColumnIndex("memo_date")
+
+            // 데이터를 가져온다.
+            val memo_subject=c1.getString(idx1)
+            val memo_date=c1.getString(idx2)
+
+//            Log.d("memo_app",memo_subject)
+//            Log.d("memo_app",memo_date)
+//            Log.d("memo_app","----------")
+
+            // 데이터를 담는다.
+            subject_list.add(memo_subject)
+            date_list.add(memo_date)
+
+            // RecyclerView에게 갱신하라고 명령한다.
+            binding.mainRecycler.adapter?.notifyDataSetChanged()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -44,5 +101,29 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    // RecyclerView의 어댑터
+    inner class MainRecyclerAdapter : RecyclerView.Adapter<MainRecyclerAdapter.ViewHolderClass>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
+            val mainRecyclerBinding = MainRecyclerRowBinding.inflate(layoutInflater)
+            val holder = ViewHolderClass(mainRecyclerBinding)
+            return holder
+        }
+
+        override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+            holder.rowMemoSubject.text=subject_list[position]
+            holder.rowMemoDate.text=date_list[position]
+        }
+
+        override fun getItemCount(): Int {
+            return subject_list.size
+        }
+        // HolderClass
+        inner class ViewHolderClass(mainRecyclerBinding: MainRecyclerRowBinding):RecyclerView.ViewHolder(mainRecyclerBinding.root) {
+            // View의 주소값을 담는다.
+            val rowMemoSubject= mainRecyclerBinding.memoSubject
+            val rowMemoDate=mainRecyclerBinding.memoDate
+        }
     }
 }
--- app/src/main/java/reb/or/kr/app1_memo/MemoAddActivity.kt
+++ app/src/main/java/reb/or/kr/app1_memo/MemoAddActivity.kt
@@ -2,8 +2,13 @@ package reb.or.kr.app1_memo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.Menu
 import android.view.MenuItem
+import android.view.inputmethod.InputMethodManager
 import reb.or.kr.app1_memo.databinding.ActivityMemoAddBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MemoAddActivity : AppCompatActivity() {
 
@@ -23,12 +28,57 @@ class MemoAddActivity : AppCompatActivity() {
         // 이전 버튼 설정
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // 쓰레드를 가동한다.
+        Thread{
+            SystemClock.sleep(500)
+            runOnUiThread{
+                // 포커스를 준다.
+                binding.addMemoSubject.requestFocus()
+                // 키보드를 보여준다.
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.showSoftInput(binding.addMemoSubject,InputMethodManager.SHOW_IMPLICIT)
+            }
+        }.start()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.add_menu,menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             // 이전 버튼
             android.R.id.home->{
+                finish()
+            }
+            // 저장 버튼
+            R.id.add_menu_write->{
+                // 사용자가 입력한 내용을 가지고 온다.
+                val memo_subject=binding.addMemoSubject.text
+                val memo_text=binding.addMemoText.text
+
+                // 쿼리문
+                val sql ="""
+                    insert into MemoTable(memo_subject,memo_text,memo_date)
+                    values(?,?,?)
+                """.trimIndent()
+
+                // 데이터베이스 오픈
+                val helper = DBHelper(this)
+
+                // 현재시간을 구한다.
+                val sdf=SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                val now = sdf.format(Date())
+
+                // ? 에 셋팅될 값
+                val arg1 = arrayOf(memo_subject,memo_text,now)
+
+                // 저장
+                helper.writableDatabase.execSQL(sql,arg1)
+
+                helper.writableDatabase.close()
                 finish()
             }
         }
